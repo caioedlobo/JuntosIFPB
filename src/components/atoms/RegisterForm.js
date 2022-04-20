d7d48fa1a4ee51fce8f2f03f396623002016b373
Caio Lobo <caioeduardolobo@gmail.com>
2022-04-20 20:02:23 -0300

    add textfield verification to login  on RegisterForm

--- src/components/atoms/RegisterForm.js
+++ src/components/atoms/RegisterForm.js
@@ -12,6 +12,15 @@ const RegisterForm = (props) => {
   const [nameFormData, setNameFormData] = useState()
   const [emailFormData, setEmailFormData] = useState()
   const [passwordFormData, setPasswordFormData] = useState()
+
+
+  const [errorEmailController, setErrorEmailController] = useState(false)
+  const [errorEmailMessage, setErrorEmailMessage] = useState("")
+  let [previousEmail, setPreviousEmail] = useState("")
+
+  const [errorPasswordController, setErrorPasswordController] = useState(false)
+  const [errorPasswordMessage, setErrorPasswordMessage] = useState("")
+  let [previousPassword, setPreviousPassword] = useState("")
   
 
   const emailLoginData = (e) => {
@@ -37,24 +46,71 @@ const RegisterForm = (props) => {
         else if(emailFormData.split('@')[1] === "ifpb.edu.br" ){
           return true
         }
+        else{
+          console.log("erro else 0")
+          errorControllerHandler("Email inválido")
+          return false
+        }
+      }
+      else{
+        console.log("erro else 1")
+        errorControllerHandler("Email inválido")
+        return false
       }
       
     }
     else if (!regEx.test(emailFormData) && emailFormData !== ""){
+      console.log("erro else if")
+      errorControllerHandler("Email inválido")
       return false
     }
     else{
-      
+      console.log("erro else 2")
+      errorControllerHandler("Email inválido")
+      return false
     }
   }
-  /* const emailValidationIF = () => {
-    
-    if( emailFormData.includes('@')){
-      emailFormData.split('@')
+  
+
+  const errorControllerHandler = (e) => {
+    if(e === "Invalid password"){
+      setErrorPasswordController(true)
+      setErrorPasswordMessage(e)
+      setPreviousPassword(passwordFormData)
+    }
+    else{
+    setErrorEmailController(true)
+    setErrorEmailMessage(e)
+    setPreviousEmail(emailFormData)
+    }
+  }
+
+  
+  const emailErrorHandler = (e) => {
+    setPreviousEmail("")
+    if (previousEmail === e){
+    setErrorEmailController(true)
+
+    }
+    else{
+      setErrorEmailController(false)
+      setErrorEmailMessage("")
     }
     
+  }
+
+  const passwordErrorHandler = (e) => {
+    setPreviousPassword("")
+    if (previousPassword === e){
+    setErrorPasswordController(true)
+
+    }
+    else{
+      setErrorPasswordController(false)
+      setErrorPasswordMessage("")
+    }
     
-  } */
+  }
   
   return (
     <form
@@ -68,26 +124,41 @@ const RegisterForm = (props) => {
         justifyContent: "center"
       }}
      onSubmit={(e) => {
+       console.log("1")
        e.preventDefault();
       
        if (emailValidation()){
-         
-        Axios.post("https://backend-juntosifpb.herokuapp.com/auth/register", {
+         console.log("teste")
+        Axios.post("http://localhost:3001/auth/register", {
           name: nameFormData,
           email: emailFormData,
           password: passwordFormData, 
         })
-      }
-     /*  else{
-        console.log("Email não é válido");
-     } */}
+        .then((response) => {console.log(response.error)})
+
+        .catch(error => {
+          console.log("error:", error.response.data.error)
+          errorControllerHandler(error.response.data.error)
+      })
+
+     }}
     }
     >
       <ImageLogin />
 
      <NameText nameLoginData={nameLoginData}/>
-      <LoginText emailLoginData={emailLoginData}/>
-      <PasswordText passwordLoginData={passwordLoginData} label={"Digite a senha"}/>
+      <LoginText 
+      emailLoginData={emailLoginData}
+      errorEmailMessage={errorEmailMessage} 
+      errorEmail={errorEmailController} 
+      errorEmailController={emailErrorHandler}
+      />
+      <PasswordText 
+      passwordLoginData={passwordLoginData} 
+      errorPasswordMessage={errorPasswordMessage} 
+      errorPassword={errorPasswordController} 
+      errorPasswordController={passwordErrorHandler}
+      label={"Digite a senha"}/>
       
       <HeightFormHandler />
       <Button type="submit"> Registrar </Button>
